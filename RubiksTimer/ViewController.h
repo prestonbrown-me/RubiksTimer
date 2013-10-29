722da3cc082f281703a3c365a80287e9b454ea25
Preston Brown <orangelettuce@gmail.com>
2013-10-28 21:27:07 -0500

    Initial Commit

---
+++ RubiksTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RubiksTimer
+//
+//  Created by Developer on 10/28/13.
+//  Copyright (c) 2013 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
